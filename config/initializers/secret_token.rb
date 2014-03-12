506af79666e6aec92919b1b29c78928b0ea52302
Nick Albino <nick.albino@gmail.com>
2014-03-12 10:08:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AndrewApp::Application.config.secret_key_base = '438de3fe628856a6e769554ea853bce13c10a153c2b640fff3e3c7a078f0e15afde7fd2275827b3f7c87a65fd1f508a8a2202f55fcf809c4915b7839d3e0a280'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AndrewApp::Application.config.session_store :cookie_store, key: '_andrew_app_session'
